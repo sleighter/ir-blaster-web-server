90eb0ee21dadb8b3bd7a105fecdb4a9c77cdbf1f
Alex Sleighter <alex@orderup.com>
2016-11-27 11:31:31 -0500

    Use socket binding in prod to avoid port restrictions

--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,22 @@ threads Integer(ENV['PUMA_THREADS']  || 8), Integer(ENV['PUMA_THREADS'] || 8)
 
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 
-rackup DefaultRackup
-port = ENV['PORT'] || 5000
-backlog = Integer(ENV['PUMA_BACKLOG'] || 20)
-environment ENV['RACK_ENV'] || 'development'
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 
-bind "tcp://0.0.0.0:#{port}?backlog=#{backlog}"
+rackup DefaultRackup
+rack_env = ENV['RACK_ENV'] || 'development'
+environment rack_env
+
+if rack_env == 'development'
+  port = ENV['PORT'] || 5000
+  backlog = Integer(ENV['PUMA_BACKLOG'] || 20)
+  bind "tcp://0.0.0.0:#{port}?backlog=#{backlog}"
+else
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+end
+
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
 
 activate_control_app
